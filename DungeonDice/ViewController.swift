634f72bfeea79f7bac59846b7afac53fbab0301a
Adrianna Zhao <zhaocx@bc.edu>
2019-06-04 16:32:04 -0400

    2.12 Dungeons and Dragons (inefficient)

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dieRoll: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func fourSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...4)
+        dieRoll.text = "You rolled a 4-sided die and got a " + String(diceRoll)
+    }
 
-
+    @IBAction func sixSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...6)
+        dieRoll.text = "You rolled a 6-sided die and got a " + String(diceRoll)
+    }
+    
+    @IBAction func eightSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...8)
+        dieRoll.text = "You rolled a 8-sided die and got a " + String(diceRoll)
+    }
+    @IBAction func tenSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...10)
+        dieRoll.text = "You rolled a 10-sided die and got a " + String(diceRoll)
+    }
+    @IBAction func twelveSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...12)
+        dieRoll.text = "You rolled a 12-sided die and got a " + String(diceRoll)
+    }
+    @IBAction func twentySidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...20)
+        dieRoll.text = "You rolled a 20-sided die and got a " + String(diceRoll)
+    }
+    @IBAction func hundredSidedDiePressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...100)
+        dieRoll.text = "You rolled a 100-sided die and got a " + String(diceRoll)
+    }
 }
 
